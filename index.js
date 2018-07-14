52a06d0a2ed5804f163774e1c85d7fe768c401a6
mark sauer-utley <mark.utley.713@gmail.com>
2018-07-14 15:06:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  let message;
+  if(distance <= 400){
+    message = "This one is on me!";
+  } else if(distance > 2500){
+    message = "No can do.";
+  } else if(distance > 2000){
+    message = "I will gladly take your thirty bucks.";
+  }
+  return message;
+}
+
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+}
+
+function switchOnCharmFromTip(tip){
+  let charm;
+  
+  switch(tip){
+    case "generous":
+      charm = "Thank you so much.";
+      break;
+    case "not as generous":
+      charm = "Thank you.";
+      break;
+    default:
+      charm = "Bye."
+  }
+  return charm;
+}
