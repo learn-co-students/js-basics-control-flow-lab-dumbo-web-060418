2db241574df6af520975d4585c1c9943a2d7aa99
QMaximillian <quinnlashinsky@gmail.com>
2018-07-14 21:39:03 -0400

    Done.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function scuberGreetingForFeet(someValue) {
 	if (someValue <= 400) {
 		return 'This one is on me!'
 	}
-	else if (someValue <= 2500) {
+	else if (someValue <= 2500 && someValue >= 2000) {
 		return 'I will gladly take your thirty bucks.'
 	} else {
 		return 'No can do.'
